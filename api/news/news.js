9b21820f0b923f678ac652e26953a12996436a43
beagle <chldusdn20@gmail.com>
2021-12-06 11:00:13 +0900

    refactor: remove unused router path

--- api/news/news.js
+++ api/news/news.js
@@ -4,12 +4,10 @@ const {upload} = require("../../config/file")
 const controller = require('./news.controller');
 
 router.post('/register', upload.array("files"), controller.create)
-router.post('/test', upload.array("files"), controller.test)
-router.get('/readAll', controller.readAll)
+router.post('/readByPage', controller.getNewsContentInPage)
 router.get('/read/:idx', controller.read)
 router.get('/addViewCount/:idx', controller.addViewCount)
 router.post('/getTotalPage', controller.getTotalPage)
-router.post('/readByPage', controller.getNewsContentInPage)
 router.post('/update', controller.update)
 
 module.exports = router;
